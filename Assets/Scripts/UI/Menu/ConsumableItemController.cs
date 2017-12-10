c346704783f47959f969d5853708a0acf6efb141
Waevka <waveswallow@gmail.com>
2017-12-10 23:18:49 +0100

    Added Item class.

--- Assets/Scripts/UI/Menu/ConsumableItemController.cs
+++ Assets/Scripts/UI/Menu/ConsumableItemController.cs
@@ -1,22 +1,44 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ConsumableItemController : MonoBehaviour {
     [SerializeField]
     GameObject itemPrefab;
-	// Use this for initialization
-	void Start () {
+    private List<GameObject> items;
+    int itemIndex;
+    [SerializeField]
+    GameObject itemSelectorPrefab;
+    private GameObject itemSelector;
+    [SerializeField]
+    List<Sprite> imageList;
+
+    //list<gameitem> hfhfhd;
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if (Input.GetButtonDown("Vertical") && items.Count > 0)
+        {
+            itemIndex = Mathf.Clamp(itemIndex + (int)Input.GetAxisRaw("Vertical"), 0, items.Count - 1);
+            UpdateActiveItem();
+        }
+    }
+
+    private void UpdateActiveItem()
+    {
+        itemSelector.transform.SetParent(items[itemIndex].transform, false);
+    }
 
     private void OnEnable()
     {
+        items = new List<GameObject>();
+        itemIndex = 0;
         foreach(Transform child in transform)
         {
             Destroy(child.gameObject);
@@ -26,7 +48,20 @@ public class ConsumableItemController : MonoBehaviour {
         {
             GameObject g = Instantiate(itemPrefab);
             g.transform.SetParent(transform, false);
+            items.Add(g);
         }
 
+        if(items.Count > 0)
+        {
+            itemSelector = Instantiate(itemSelectorPrefab);
+            itemSelector.transform.SetParent(items[itemIndex].transform, false);
+        }
+
+        TestFill();
+    }
+
+    private void TestFill()
+    {
+        items[0].GetComponent<Item>().Initialize("Healing", imageList[0], ItemType.CONSUMABLE);
     }
 }
---
+++ Assets/Scripts/UI/Menu/Item.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum ItemType { CONSUMABLE, HEALTH, SPEED, WEAPON };
+
+public class Item : MonoBehaviour {
+    [SerializeField]
+    Sprite itemImage;
+    [SerializeField]
+    string itemName;
+    [SerializeField]
+    Text itemNameTextField;
+    [SerializeField]
+    Image itemImageField;
+    [SerializeField]
+    Image itemIconField;
+    [SerializeField]
+    ItemType itemType;
+    //itemType
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Initialize(string _itemName, Sprite _itemImage, ItemType _type)
+    {
+        itemName = _itemName;
+        itemImage = _itemImage;
+        itemType = _type;
+        itemImageField.sprite = itemImage;
+        //TODO: set icon by type
+        itemNameTextField.text = itemName;
+    }
+}
