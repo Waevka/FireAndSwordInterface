7c6db7d06f227f5dc48eda3d10a7dec4975df2ea
Waevka <waveswallow@gmail.com>
2017-12-10 22:35:08 +0100

    Added dynamic UI items.

---
+++ Assets/Scripts/UI/Menu/ConsumableItemController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConsumableItemController : MonoBehaviour {
+    [SerializeField]
+    GameObject itemPrefab;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnEnable()
+    {
+        foreach(Transform child in transform)
+        {
+            Destroy(child.gameObject);
+        }
+        
+        for(int i = 0; i < 5; i++)
+        {
+            GameObject g = Instantiate(itemPrefab);
+            g.transform.SetParent(transform, false);
+        }
+
+    }
+}
