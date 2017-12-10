f5d76a0923f0a6fa068e276d234470ec4ef7d9e5
Waevka <waveswallow@gmail.com>
2017-12-10 22:21:41 +0100

    Fixed scaling problems when changing screen size.

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -3,15 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
-
+    [SerializeField]
+    GameObject menuPanels;
+    [SerializeField]
+    bool isControllerActive;
 	// Use this for initialization
 	void Start () {
-		
+        isControllerActive = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        transform.Rotate(Vector3.up, Input.GetAxis("Horizontal") * 2.0f);
-        transform.Translate(Vector3.forward * Input.GetAxis("Vertical") * 0.3f);      
+        if (isControllerActive)
+        {
+            transform.Rotate(Vector3.up, Input.GetAxis("Horizontal") * 2.0f);
+            transform.Translate(Vector3.forward * Input.GetAxis("Vertical") * 0.3f);
+        }
+        if (Input.GetButtonDown("Menu"))
+        {
+            isControllerActive = menuPanels.activeSelf;
+            menuPanels.SetActive(!menuPanels.activeSelf);
+        }
+        
 	}
 }
---
+++ Assets/Scripts/UI/Menu/MenuController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour {
+    [SerializeField]
+    int currentActiveTab;
+    [SerializeField]
+    GameObject[] tabLabels;
+    [SerializeField]
+    GameObject[] tabs;
+    private void Awake()
+    {
+        currentActiveTab = 0;
+        if (isActiveAndEnabled)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+    // Use this for initialization
+    void Start () {
+    }
+
+    private void OnEnable()
+    {
+        currentActiveTab = 0;
+        UpdateActiveTabs();
+        //reset menu state
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if(Input.GetButtonDown("Horizontal"))
+        {
+            currentActiveTab = Mathf.Clamp(currentActiveTab + (int)Input.GetAxisRaw("Horizontal"), 0, tabLabels.Length-1);
+            UpdateActiveTabs();
+        }
+	}
+
+    void UpdateActiveTabs()
+    {
+        for(int i = 0; i < tabLabels.Length; i++)
+        {
+            if(i == currentActiveTab)
+            {
+                tabLabels[i].GetComponent<Image>().color = Color.white;
+                tabs[i].SetActive(true);
+            }
+            else
+            {
+                tabLabels[i].GetComponent<Image>().color = Color.black;
+                tabs[i].SetActive(false);
+            }
+        }
+    }
+}
